12b4fc18122c896766bf222a4b3db2eca71b33ef
Chuck Zumbaugh <zumbaughca@gmail.com>
2021-04-26 21:39:47 -0500

    modify print

--- plots.py
+++ plots.py
@@ -4,7 +4,7 @@ import plotly.express as px
 def create_choropleth(df: pd.DataFrame, color_data, title):
     print(df.columns)
     print(df.geometry.head())
-    print(df.index.head())
+    print(df.index)
     fig = px.choropleth(data_frame=df, locations=df.index,
                         geojson=df.geometry,
                         color=color_data,
